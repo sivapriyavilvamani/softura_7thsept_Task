ef1926c8fa1392c357703f5b77f4f18e9d3f11ee
sivapriyavilvamani <sivapriyavilvamani@gmail.com>
2022-09-09 21:15:55 +0530

    sept 9 task4

---
+++ AbstractClassTask.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+    
+{
+    abstract class Operations
+    {
+        public abstract void Calculate(int num1,int num2);
+
+    }
+     class Add: Operations
+        {
+            public override void  Calculate(int num1,int num2)        
+            {                
+                Console.WriteLine("The sum of two numbers is {0}", num1 + num2);
+            }
+    }
+    class sub : Operations
+    {
+        public override void Calculate(int num1, int num2)
+        {
+           Console.WriteLine("The difference of two numbers is {0}", num1 - num2);
+        }
+    }
+    class Multiply : Operations
+    {
+        public override void Calculate(int num1, int num2)
+        {
+            Console.WriteLine("The Multiple of two numbers is {0}", num1 * num2);
+        }
+    }
+    class divide : Operations
+    {
+        public override void Calculate(int num1, int num2)
+        {
+           Console.WriteLine("The division of two numbers is {0}", num1 / num2);
+        }
+    }
+    internal class AbstractClassTask
+    {
+        public static void Main()
+        {
+            Add add = new Add();
+            sub diff=new sub();
+            Multiply multi = new Multiply();
+            divide division = new divide();
+            add.Calculate(25,65);
+            diff.Calculate(25, 65);
+            multi.Calculate(25, 65);
+            division.Calculate(755, 5);
+
+        }
+    }
+}
---
+++ InterfaceTask.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Accomadation : Membership
+    {
+        public void silver()
+        {
+            Console.WriteLine("3 days accomodation in Resort");
+                
+         }
+        public void gold()
+        {
+            Console.WriteLine("5 days accomodation in Resort and 2 dinners at house");
+
+        }
+        public void platinum()
+        {
+            Console.WriteLine("7 days accomodation in Resort and 5 dinners at house");
+
+        }
+    }
+    internal class InterfaceTask
+    {
+        public static void Main()
+        {
+            Accomadation stay = new Accomadation();
+            stay.silver();
+            stay.gold();
+            stay.platinum();
+        }
+
+    }
+}

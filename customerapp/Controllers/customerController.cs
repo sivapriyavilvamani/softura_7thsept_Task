d7ade7c3cf5b33932b76d1509758eb95a2247301
sivapriyavilvamani <113021313+sivapriyavilvamani@users.noreply.github.com>
2022-10-03 12:49:18 +0530

    Add files via upload

---
+++ customerapp/Controllers/customerController.cs
@@ -0,0 +1,22 @@
+﻿using customerapp.model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace customerapp.Controllers
+{
+    public class customerController : Controller
+    {
+        public string Index()
+        {
+            return "Customer";
+        }
+        public IActionResult customerinfo()
+        {
+            List<customerClass> licus = customerClass.GetAllcustomer();
+            return View(licus);
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
---
+++ customerapp/Models/customerClass.cs
@@ -0,0 +1,26 @@
+﻿namespace customerapp.model
+{
+    public class customerClass
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public static List<customerClass> GetAllcustomer()
+        {
+            List<customerClass> customers = new()
+        {
+        new customerClass() { Id=1, Name= "priya" },
+        new customerClass() { Id=2, Name= "swathi" },
+        new customerClass() { Id=3, Name= "swetha" },
+        new customerClass() { Id=4, Name= "kanmani" }
+      
+
+        };
+
+            return customers;
+        }
+
+
+
+    }
+}

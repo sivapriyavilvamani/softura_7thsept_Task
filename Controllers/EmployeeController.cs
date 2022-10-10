5dec3193bd013f846898a77c975f88329447f8eb
sivapriyavilvamani <113021313+sivapriyavilvamani@users.noreply.github.com>
2022-10-10 17:45:46 +0530

    Add files via upload

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EmploeeTask1.Models;
+
+
+namespace EmploeeTask1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult EmployeeInfo()
+        {
+           Employee employee = Employee.GetEmployeeInfo();
+            return View(employee);
+        }
+        public IActionResult allEmployeeInfo()
+        {
+            List<Employee> liemp = Employee.GetAllEmployee();
+            return View(liemp);
+        }
+        public IActionResult Insert()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,38 @@
+﻿namespace EmploeeTask1.Models
+{
+    public class Employee
+    {
+       
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+
+            public string Gender { get; set; }
+
+
+            public static List<Employee> GetAllEmployee()
+
+            {
+                List<Employee> employee = new()
+        {
+        new Employee() { Id=1, FirstName= "Sivapriya", LastName="Vilvamani",Gender="Female" },
+        new Employee()  { Id=2, FirstName= "Lavanya", LastName="Sivakumar",Gender="Female" },
+        new Employee()  { Id=3, FirstName= "Fahad", LastName="Fasil",Gender="Male" },
+        new Employee()  { Id=4, FirstName= "Kamal", LastName="Hassan",Gender="Male" } ,
+        new Employee() { Id=5, FirstName= "Ramya", LastName="Vilvamani", Gender="Female" }
+
+        };
+
+                return employee;
+            }
+
+            public static Employee GetEmployeeInfo()
+            {
+                Employee emp = new Employee { Id = 1, FirstName = "Rekha", LastName = "Vikram", Gender = "Female" };
+                return emp;
+
+            }
+        }
+    }
+
